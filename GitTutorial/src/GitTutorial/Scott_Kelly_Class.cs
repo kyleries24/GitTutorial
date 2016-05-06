9ea136ca7ec5974e46a004347a329d7f0aa1f78b
Scott Kelly <sctkelly@gmail.com>
2016-05-06 09:53:18 -0400

    Added Scott Kelly Class.

---
+++ GitTutorial/src/GitTutorial/Scott_Kelly_Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitTutorial
+{
+    public class Scott_Kelly_Class
+    {
+        public int AwesomenessRatingFromOneToTen { get; set; }
+    }
+}
